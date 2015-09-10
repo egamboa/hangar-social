3be9fb831c0e006624d4d951b3c382e59efab21c
egamboa <estebang@thehangar.cr>
2015-09-10 11:48:30 -0600

    la calidad del helado

--- client/templates/hangar-cocinando/hangar-cocinando.js
+++ client/templates/hangar-cocinando/hangar-cocinando.js
@@ -1,3 +1,65 @@
 Template.cocinando.rendered = function () {
 	$('.fancybox').fancybox();
+	$('#fullpage').fullpage();
 };
+
+Template.cocinando.helpers({
+	recepies_p1: function () {
+		return [
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Gallo Pinto',
+				'subtitle': 'gah-yoh pin-toh'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Sopa Negra',
+				'subtitle': 'sou-pah neh-grah'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Gallo Pinto',
+				'subtitle': 'gah-yoh pin-toh'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Sopa Negra',
+				'subtitle': 'sou-pah neh-grah'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Gallo Pinto',
+				'subtitle': 'gah-yoh pin-toh'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Sopa Negra',
+				'subtitle': 'sou-pah neh-grah'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Gallo Pinto',
+				'subtitle': 'gah-yoh pin-toh'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Sopa Negra',
+				'subtitle': 'sou-pah neh-grah'
+			},
+			{
+				'thumbnail': 'images/cocinando/dish.png',
+				'recepie_img': 'http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_b.jpg',
+				'name': 'Sopa Negra',
+				'subtitle': 'sou-pah neh-grah'
+			}
+		]
+	}
+});
