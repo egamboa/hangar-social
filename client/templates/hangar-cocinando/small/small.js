3f0b6d2e3a3289a64d5b937bd6842e2708679ff7
egamboa <estebang@thehangar.cr>
2015-10-06 15:33:51 -0600

    small fixes for carousel

--- client/templates/hangar-cocinando/small/small.js
+++ client/templates/hangar-cocinando/small/small.js
@@ -1,9 +1,11 @@
 Template['cookSmall'].__helpers = Template['cocinando'].__helpers;
 
 Template.cookSmall.rendered = function () {
-	$('#recipesSlide').slick({
-		arrows: false
-	});
+	setTimeout(function(){
+		$('#recipesSlide').fadeIn(400).slick({
+			arrows: false
+		});
+	}, 300);
 };
 
 Template.cookSmall.events({
