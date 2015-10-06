77836439366be13768ccaf8282f3dd258532a1e8
etb811ac <etb811ac@aol.com>
2015-10-06 11:43:26 -0600

    ups

--- client/templates/hangar-cocinando/small/small.js
+++ client/templates/hangar-cocinando/small/small.js
@@ -5,3 +5,12 @@ Template.cookSmall.rendered = function () {
 		arrows: false
 	});
 };
+
+Template.cookSmall.events({
+	'click div.topContent': function () {
+		$('html, body').animate({
+			scrollTop: $("div.recipe").offset().top
+		}, 1000)
+	}
+});
+
