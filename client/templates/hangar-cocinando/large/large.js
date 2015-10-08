b433800f66217faf35ac2540046af1c745a9e2f7
etb811ac <etb811ac@aol.com>
2015-10-08 14:31:42 -0600

    ahora si papa

--- client/templates/hangar-cocinando/large/large.js
+++ client/templates/hangar-cocinando/large/large.js
@@ -1,1 +1,13 @@
 Template['cookLarge'].__helpers = Template['cocinando'].__helpers;
+
+$('.arrow').on('click', function(){
+	
+})
+
+Template.cookLarge.events({
+	'click div.arrow': function () {
+		$('html, body').animate({
+			scrollTop: $("div.allRecipes").offset().top
+		}, 1000);
+	}
+});
