ee254e6190e460025b46511275ce1b10e4c06907
egamboa <estebang@thehangar.cr>
2015-10-08 15:49:04 -0600

    changes

--- client/templates/hangar-cocinando/small/small.js
+++ client/templates/hangar-cocinando/small/small.js
@@ -1,23 +1,6 @@
 Template['cookSmall'].__helpers = Template['cocinando'].__helpers;
 
 Template.cookSmall.rendered = function () {
-	setTimeout(function(){
-		$('#recipesSlide').fadeIn(300).slick({
-			arrows: false,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			centerMode: true,
-			variableWidth: true
-		});
-
-		$('#cookSmall').on('click', '.smallSeeRecipe', function(event){
-			Meteor.call("getCurrentRecipe", $(event.target).attr('data-id'), function (error, result) { 
-				Session.set('currentRecipe', result);
-				$('body').addClass('noscroll');
-				$('#currentRecipe').addClass('opened');
-	    	});
-		});
-	}, 300);
 };
 
 Template.cookSmall.events({
